9449ab74bf573b880eeae1a8a7c8554a7a35bc1b
YaelZamora <ajedrezkasparov@gmail.com>
2023-05-29 13:23:01 -0600

    Initial Commit

---
+++ EnterFES/EnterFESApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnterFESApp.swift
+//  EnterFES
+//
+//  Created by Yael Javier Zamora Moreno on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct EnterFESApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
